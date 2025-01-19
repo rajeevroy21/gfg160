efb4262579b980df4411ede084ae3b9a46b3cffa
Rajeev kumar <115883777+rajeevroy21@users.noreply.github.com>
2025-01-19 22:48:39 +0530

    Create Day26.cpp

---
+++ Day26.cpp
@@ -0,0 +1,16 @@
+int minRemoval(vector<vector<int>> &intervals) {
+        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
+        int firstEnd = intervals[0][1];
+        int count = 0;
+        for (int i = 1; i < n; i++){
+            if (intervals[i][0] >= firstEnd){
+                firstEnd = intervals[i][1];
+            }
+            else{
+                firstEnd = min(firstEnd, intervals[i][1]);
+                count++;
+            }
+        }
+        return count;
+    }
